659e67dc502091432f27ad53cf583a9769f89b35
ideumi <ideumi.crystal086@passmail.net>
2026-05-13 09:29:24 +0200

    update version date

--- internal/constants/core.go
+++ internal/constants/core.go
@@ -9,7 +9,7 @@ package constants
 import "runtime"
 
 const (
-	VERSION_DATE = "2026-05-11"
+	VERSION_DATE = "2026-05-13"
 	HIST_FILE    = ".ChipLangHistory"
 
 	RR_CONTEXT_DISPLAY_NAME     = "<ChipLangProgram>"
