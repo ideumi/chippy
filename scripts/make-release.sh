164e11fa4bf67bc29dd2904d58f16067bc7e5d7e
ideumi <ideumi.crystal086@passmail.net>
2026-06-24 18:05:27 +0200

    bump to 1.0.22

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -3,7 +3,7 @@ set -e
 
 # Configuration
 RELEASE_NAME="chippy"
-VERSION="1.0.21"
+VERSION="1.0.22"
 ARCH=${TARGET_ARCH:-$(uname -m)}
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 RELEASE_DIR="${RELEASE_NAME}-${VERSION}-${OS}-${ARCH}"
