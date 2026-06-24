e6cd98125afe7b900cd013f9eca0cfa3894bab11
ideumi <ideumi.crystal086@passmail.net>
2026-06-24 18:06:34 +0200

    bump VERSION_DATE

--- internal/constants/core.go
+++ internal/constants/core.go
@@ -9,7 +9,7 @@ package constants
 import "runtime"
 
 const (
-	VERSION_DATE = "2026-06-23"
+	VERSION_DATE = "2026-06-24"
 	HIST_FILE    = ".ChippyHistory"
 
 	RR_CONTEXT_DISPLAY_NAME     = "<ChippyProgram>"
